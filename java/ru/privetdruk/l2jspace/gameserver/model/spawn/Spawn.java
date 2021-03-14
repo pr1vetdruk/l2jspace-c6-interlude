19e098e9ab08267831d53e76c9304b5678f73ccb
pr1vetdruk <privetdruk.inc@gmail.com>
2021-03-14 22:22:13 +0300

    [Synchronization with L2jMobius] Increased spawn z values by a fixed value (64) to fix spawn zone issues.

--- java/ru/privetdruk/l2jspace/gameserver/model/spawn/Spawn.java
+++ java/ru/privetdruk/l2jspace/gameserver/model/spawn/Spawn.java
@@ -414,7 +414,8 @@ public class Spawn {
 
         // Correct Z of monsters. Do not correct Z of flying NPCs.
         if (npc.isMonster() && !npc.isFlying()) {
-            final int geoZ = GeoEngine.getInstance().getHeight(newlocx, newlocy, newlocz);
+            int geoZ = GeoEngine.getInstance().getHeight(newlocx, newlocy, newlocz) + 64;
+
             // Do not correct Z distances greater than 300.
             if (Util.calculateDistance(newlocx, newlocy, newlocz, newlocx, newlocy, geoZ, true) < 300) {
                 newlocz = geoZ;
